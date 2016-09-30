62a0c45912847fffa70ea705cd824cd73cb30c02
Jotasaez <jotasaez@hotmail.com>
2016-09-30 02:09:25 -0300

    Update simpleCart2kamillu.js

--- simpleCart2kamillu.js
+++ simpleCart2kamillu.js
@@ -432,7 +432,7 @@ itemsString +="\n\nFORMA DE PAGO: " + formapago;
 var form = document.createElement("form"); 
 form.style.display = "none"; 
 form.method = "POST"; 
-form.action = "http://kamillustore.16mb.com/email.php";
+form.action = "http://kamillu.ueuo.com/email.php";
 form.acceptCharset = "utf-8"; 
 form.appendChild(this.createHiddenElement("jcitems", itemsString)); 
 form.appendChild(this.createHiddenElement("jcremite", remite)); 
