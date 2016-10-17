f17e2d8a50d2fcc2db0560da18a3e8cf9f89515a
Jotasaez <jotasaez@hotmail.com>
2016-10-17 00:43:31 -0400

    Create kamilluPromo.js

---
+++ kamilluPromo.js
@@ -0,0 +1,9 @@
+<script>
+function myFunction() {
+var x = document.getElementById("field1");
+    x.disabled = true;
+    document.getElementById("precio").innerHTML = 
+
+document.getElementById("field1").value;
+}
+</script>
