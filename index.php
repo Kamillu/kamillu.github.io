c4ca51517c2feb7203c088e98ef11989f86aba4d
Jotasaez <jotasaez@hotmail.com>
2016-11-09 17:38:31 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,5 +3,8 @@
   
 <meta http-equiv="Refresh" content="0;url=https://kamillustore.blogspot.com/">
 </head>
-  
+ <center>
+<a href="https://www.facebook.com/dialog/pagetab?app_id=221165111427900&amp;next=https://apps.facebook.com/kamillustore" target="_blank">
+<span style="font-size: small;">Agregar a Mi Página de Facebook</span></a></center>
+
 </html>
