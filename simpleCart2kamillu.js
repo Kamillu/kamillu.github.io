ac65d6518db4b545ac8417c5630d3c3641c0fac0
Jotasaez <jotasaez@hotmail.com>
2016-12-15 00:02:41 -0300

    Update simpleCart2kamillu.js

--- simpleCart2kamillu.js
+++ simpleCart2kamillu.js
@@ -299,17 +299,7 @@ function Cart(){
 			form.appendChild( me.createHiddenElement( "amount_"			+ counter, item.price		) );
 			form.appendChild( me.createHiddenElement( "item_number_"	+ counter, counter			) );
 			
-			var option_count = 0;
-
-			me.each( item , function( value, x , field ){
-				if( field !== "id" && field !== "price" && field !== "quantity" && field !== "name" && field !== "shipping" && option_count < 10) {
-					form.appendChild( me.createHiddenElement( "on" + option_count + "_"	+ counter, 	field ) );
-					form.appendChild( me.createHiddenElement( "os" + option_count + "_"	+ counter, 	value ) );
-					option_count++;
-				}
-			});
-
-			form.appendChild( me.createHiddenElement( "option_index_" + counter, option_count) );
+			
 
 		});
 
